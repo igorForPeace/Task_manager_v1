46eb1b4a712770874957f2be4132706d5e1735ca
igorForPeace <igor29319965@gmail.com>
2023-08-25 10:47:23 +0300

    Add some new Info

--- main.cpp
+++ main.cpp
@@ -10,6 +10,7 @@
 int main(int argc, char *argv[])
 {
     QApplication a(argc, argv);
+    qDebug()<<"Start the program";
     EnterWindow enterWindow;
     QString accPath;
     if(enterWindow.exec()!=QDialog::Accepted)
